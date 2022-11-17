cc48ab69a679c9f32d3c5b866fa09bbe31ea792e
Dell <chhipashehjad10@gmail.com>
2022-11-17 16:12:06 +0530

    modified_by_shehjad

--- p1.c
+++ p1.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 void sum();
+void sub();
 int main()
 {
 	printf("in main...\n");
 	sum();
+	sub();
 }
 void sum()
 {
 	printf("in sum...\n");
 	int x=10,y=20,res;
 	printf("res=%d\n",x+y);
+}
+void sub()
+{
+	printf("in sub...\n");
+	int x=20,y=10,res;
+	printf("res=%d\n",x-y);
 }
